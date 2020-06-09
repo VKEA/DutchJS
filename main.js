1f7ffada09a5941f1e79103dd28597c07cea7054
Zt-freak <bnkho98@hotmail.com>
2020-06-09 12:15:07 +0200

    bug fix

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import HTOTDocument from './Classes/DOM/HTMLDocument.js'
 import HTOTElement from './Classes/DOM/HTMLElement.js'
 
 import Reeks from './Classes/JS/Array.js'
-import Reeks from './Classes/JS/Boolean.js'
+import Boolean from './Classes/JS/Boolean.js'
